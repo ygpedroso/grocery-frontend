c204178b4513aae29b49bcb9d244454b6c007c89
partner <partner@aleph.engineering>
2018-08-07 15:14:10 -0400

    Export each product store action independently

--- src/redux/actions/products/index.js
+++ src/redux/actions/products/index.js
@@ -6,40 +6,38 @@ import {
     updateProduct,
 } from './../../../api/apollo/product/mutations';
 
-export default class {
-    static getProducts = () => dispatch => {
-        getProducts().then(data => {
-            dispatch({
-                type: ProductTypes.GET_PRODUCTS,
-                products: data.products,
-            });
+export const getProductsAction = () => dispatch => {
+    getProducts().then(data => {
+        dispatch({
+            type: ProductTypes.GET_PRODUCTS,
+            products: data.products,
         });
-    };
+    });
+};
 
-    static createProduct = name => dispatch => {
-        createProduct(name).then(data => {
-            dispatch({
-                type: ProductTypes.CREATE_PRODUCT,
-                product: data.createProduct.product,
-            });
+export const createProductAction = name => dispatch => {
+    createProduct(name).then(data => {
+        dispatch({
+            type: ProductTypes.CREATE_PRODUCT,
+            product: data.createProduct.product,
         });
-    };
+    });
+};
 
-    static removeProduct = productId => dispatch => {
-        removeProduct(productId).then(() => {
-            dispatch({
-                type: ProductTypes.REMOVE_PRODUCT,
-                productId,
-            });
+export const removeProductAction = productId => dispatch => {
+    removeProduct(productId).then(() => {
+        dispatch({
+            type: ProductTypes.REMOVE_PRODUCT,
+            productId,
         });
-    };
+    });
+};
 
-    static updateProduct = (productId, name) => dispatch => {
-        updateProduct(productId, name).then(data => {
-            dispatch({
-                type: ProductTypes.UPDATE_PRODUCT,
-                product: data.updateProduct.product,
-            });
+export const updateProductAction = (productId, name) => dispatch => {
+    updateProduct(productId, name).then(data => {
+        dispatch({
+            type: ProductTypes.UPDATE_PRODUCT,
+            product: data.updateProduct.product,
         });
-    };
-}
+    });
+};
