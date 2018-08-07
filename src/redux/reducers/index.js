71e6cb8dc94fbe928402b0a6f757c3b5e55a2783
partner <partner@aleph.engineering>
2018-08-07 00:18:04 -0400

    Register the products reducer in the store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import config from './config';
+import products from './products';
 
 const rootReducer = combineReducers({
     config,
+    products,
 });
 
 export default rootReducer;
