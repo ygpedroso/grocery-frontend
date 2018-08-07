bdef5aa9039976f62865803273ac4e3eefc19ee2
partner <partner@aleph.engineering>
2018-08-07 11:18:20 -0400

    Provide fetch library to apollo client

--- src/api/apollo/client.js
+++ src/api/apollo/client.js
@@ -1,7 +1,9 @@
 import ApolloClient from 'apollo-boost';
+import fetch from 'unfetch';
 
 const client = new ApolloClient({
     uri: 'http://localhost:8000/api/',
+    fetch,
 });
 
 export default client;
