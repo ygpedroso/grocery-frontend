c9912d21f9ccba364e2311f16a3003cc919352b7
partner <partner@aleph.engineering>
2018-08-07 19:11:11 -0400

    Obtain the graphql api endpoint from an environment variable

--- src/api/apollo/client.js
+++ src/api/apollo/client.js
@@ -1,8 +1,14 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'unfetch';
 
+let graphqlEndpoint = 'http://localhost:8000/api/';
+
+if (process.env.NODE_ENV === 'production') {
+    graphqlEndpoint = process.env.GROCERY_API;
+}
+
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/api/',
+    uri: graphqlEndpoint,
     fetch,
 });
 
