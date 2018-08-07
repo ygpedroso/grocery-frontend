7757a06dff680736dd5462ceabe6d1a3e4ef7e19
partner <partner@aleph.engineering>
2018-08-07 14:41:55 -0400

    Display mock data in the App layout

--- src/layouts/app/index.js
+++ src/layouts/app/index.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import ProductComponent from './../../components/product';
 import './App.css';
 
 class App extends Component {
     render() {
-        return <div className="App" />;
+        return (
+            <div className="App">
+                <div className="products-list-header">
+                    <h2>Grocery List</h2>
+                </div>
+                <div className="container products-list-container">
+                    <Grid componentClass="products-list-form">
+                        <Row className="show-grid">
+                            <Col xs={12} sm={10}>
+                                <input
+                                    type="text"
+                                    name="item"
+                                    placeholder="Add new product..."
+                                    required
+                                />
+                            </Col>
+                            <Col xs={12} sm={2}>
+                                <button>Add</button>
+                            </Col>
+                        </Row>
+                    </Grid>
+                    <ul className="products-list">
+                        <ProductComponent name="Buy Milk" />
+                        <ProductComponent name="Buy eggs" />
+                        <ProductComponent name="Pick up soda" />
+                    </ul>
+                </div>
+            </div>
+        );
     }
 }
 
