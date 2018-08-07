a54ecf04023ffc378c6807a61b4b911f2cf7a587
partner <partner@aleph.engineering>
2018-08-07 19:49:34 -0400

    Hardcode the graphql endpoint

--- src/api/apollo/client.js
+++ src/api/apollo/client.js
@@ -4,7 +4,7 @@ import fetch from 'unfetch';
 let graphqlEndpoint = 'http://localhost:8000/api/';
 
 if (process.env.NODE_ENV === 'production') {
-    graphqlEndpoint = process.env.GROCERY_API;
+    graphqlEndpoint = 'https://rideco-grocery.herokuapp.com/api/';
 }
 
 const client = new ApolloClient({
