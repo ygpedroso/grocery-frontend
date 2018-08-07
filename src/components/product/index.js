a58de63e52178556053d19a6a3865eb4b24ce729
partner <partner@aleph.engineering>
2018-08-07 18:26:53 -0400

    Remove product on user action click

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './Product.css';
+import { removeProductAction } from '../../redux/actions/products';
 
-const Product = props => <li>{props.name}</li>;
+class Product extends Component {
+    removeProduct = () => {
+        this.props.removeProductAction(this.props.productId);
+    };
+
+    render() {
+        return <li onClick={this.removeProduct}>{this.props.name}</li>;
+    }
+}
 
 Product.propTypes = {
     name: PropTypes.string.isRequired,
+    productId: PropTypes.string.isRequired,
+    removeProductAction: PropTypes.func.isRequired,
 };
 
-export default Product;
+const dispatchedActions = {
+    removeProductAction,
+};
+
+export default connect(
+    null,
+    dispatchedActions
+)(Product);
